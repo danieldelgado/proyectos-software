3547020c8d3c73c8f2a3df86d8c78c6e39790c6e
danieldelgado22g@gmail.com <danieldelgado22g@gmail.com@244fdcff-bacd-6254-7a59-d87bfe48386a>
2013-01-22 05:27:33 +0000

    ya funciona el query de many to amny solo se usa "join"

--- sw/hsd-componet/hsd-dao/src/main/java/com/vst/hsd/dao/impl/ColumnaDAOImpl.java
+++ sw/hsd-componet/hsd-dao/src/main/java/com/vst/hsd/dao/impl/ColumnaDAOImpl.java
@@ -11,14 +11,13 @@ import com.vst.util.DAO;
 @Repository("ColumnaDAO")
 public class ColumnaDAOImpl extends DAO<Columna> implements ColumnaDAO {
 
+	@SuppressWarnings("unchecked")
 	public List<Columna> buscarPorLista(Integer id) {
-		sqlQuery = "select new Columna(c.id, c.addColumn, c.ancho, c.atributo, c.cabecera, c.codigo, c.tabla, " +
-				" c.alineacion, c.formato_tipo, c.mapping,c.visible, c.orden, c.estado, c.activo)  " +
-				" from Columna c  fetch join  c.listas l where l.id=:lista";
+		sqlQuery = "select  new Columna(c.id, c.addColumn, c.ancho, c.atributo, c.cabecera, c.codigo, c.tabla,  c.alineacion, c.formato_tipo, c.mapping,c.visible, c.orden, c.estado, c.activo)  from Lista ls join  ls.columnas c where ls.id = :lista";
 		q=em.createQuery(sqlQuery);
 		q.setParameter("lista", id);	
 		try {
-			List<Columna> columnas = q.getResultList();		
+			List<Columna> columnas = q.getResultList();					
 			return columnas;
 		} catch (Exception e) {
 			e.printStackTrace();
